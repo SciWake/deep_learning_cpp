c3fad17525560baeeb0d048505786944b055f6b7
SciWake <SciWake@gmail.com>
2023-11-05 07:42:21 +0600

    Fix code style and bug fix

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 double WeightSum(const std::vector<double>& a, const std::vector<double>& b) {
     assert(a.size() == b.size());
     double output = 0.0;
-    for (int i = 0; i <= a.size(); ++i) {
+    for (int i = 0; i < a.size(); ++i) {
         output += a[i] * b[i];
     }
     return output;
@@ -20,11 +20,9 @@ std::vector<double> VectorMatMul(const std::vector<double>& numbers, const std::
     return output;
 }
 
-
-std::vector<double> NeuralNetwork(double input, const std::vector<double>& weight) {
+std::vector<double> NeuralNetwork(const std::vector<double>& input, const std::vector<std::vector<double>>& weight) {
     return VectorMatMul(input, weight);
 }
-
 
 void Print(const std::vector<double>& vector_data) {
     for (const double& number : vector_data) {
@@ -33,10 +31,10 @@ void Print(const std::vector<double>& vector_data) {
 }
  
 int main() {
-    int pass;
-    const std::vector<std::vector<double>> weights = {{0.1, 0.1, -0.3}, 
-                                            {0.1, 0.2, 0.0}, 
-                                            {0.0, 1.3, 0.1}};
+    int pass;                                        // игр, % игр, число болельщиков
+    const std::vector<std::vector<double>> weights = {{0.1, 0.1, -0.3}, //-> травмы?
+                                                      {0.1, 0.2, 0.0}, //-> победа?
+                                                      {0.0, 1.3, 0.1}}; //-> эмоциональное состояние?
     const std::vector<double> toes = {8.5, 9.5, 10.0, 0.9};  // текущее среднее число игр, сыгранных игроками
     const std::vector<double> wlrec = {0.65, 0.8, 0.8, 0.9};  // текущая доля игр, окончившихся победой(процент)
     const std::vector<double> nfans = {1.2, 1.3, 0.5, 1.0};  // число болельщиков (в миллионах)
